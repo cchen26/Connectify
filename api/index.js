cbdfda8faf3345f28a7907dbf1cd6e2b571f1a79
Chao Chen <chaochen234@gmail.com>
2023-05-01 18:46:54 -0400

    initial setup

---
+++ api/index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log("Error connecting to MongoDB", err);
+  });
+
+//Middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+app.get("/", (req, res) => {
+  res.send("welcome to homepage");
+});
+
+app.listen(8800, () => {
+  console.log("Backend server running");
+});
