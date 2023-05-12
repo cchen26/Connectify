aba2a437c92df7b291f2da006aebd1b406e59638
Chao Chen <chaochen234@gmail.com>
2023-05-12 16:18:02 -0400

    Added assets and updated index.js (server)

--- api/index.js
+++ api/index.js
@@ -5,6 +5,10 @@ import dotenv from "dotenv";
 import helmet from "helmet";
 import morgan from "morgan";
 import userRoute from "./routes/users.js";
+import authRoute from "./routes/auth.js";
+import postRoute from "./routes/posts.js";
+import path from "path";
+const router = express.Router();
 
 dotenv.config();
 
@@ -19,18 +23,35 @@ mongoose
   .catch((err) => {
     console.log("Error connecting to MongoDB", err);
   });
+app.use("/images", express.static(path.join(__dirname, "public/images")));
 
 //Middleware
 app.use(express.json());
 app.use(helmet());
 app.use(morgan("common"));
 
-app.get("/", (req, res) => {
-  res.send("welcome to homepage");
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "public/images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.name);
+  },
 });
 
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  try {
+    return res.status(200).json("File uploded successfully!");
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
+app.use("/api/posts", postRoute);
 
 app.listen(8800, () => {
-  console.log("Backend server running");
+  console.log("Backend server running!");
 });
