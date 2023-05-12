a505ceefa4456fb4cfb710beb97a578b6580feb7
Chao Chen <chaochen234@gmail.com>
2023-05-11 20:07:20 -0400

    Updated index.js to be compatible w react 18+

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,14 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import App from "./App";
 import { AuthContextProvider } from "./context/AuthContext";
 
-ReactDOM.render(
+const rootElement = document.getElementById("root");
+
+createRoot(rootElement).render(
   <React.StrictMode>
     <AuthContextProvider>
       <App />
     </AuthContextProvider>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
